64b9555f26774d20cab60f89596120046a8715fe
YeeKaiKai <kyle910204@gmail.com>
2022-12-25 00:10:07 +0800

    feat: Add update method for four functions

---
+++ models/messageBoard/updateComment_model.js
@@ -0,0 +1,24 @@
+const connect = require("../connection_db.js");
+
+/**
+ * 
+ * @param {{MContent: string, StuID: string, MNumber: int}} comment 
+ * @returns 
+ */
+module.exports = function updateComment(comment) {
+    let result = {};
+    return new Promise((resolve, reject) => {
+        connect.query(`UPDATE MESSAGE SET MContent = ? WHERE StuID = ? AND MNumber = ?`, [comment.MContent, comment.StuID, comment.MNumber], (err) => {
+            if(err) {
+                result.status = "Failed!";
+                result.message = "更改留言內容失敗！";
+                reject(result);
+                return;
+            }
+            result.status = "Success!";
+            result.message = "更改留言內容成功！";
+            resolve(result);
+            return;
+        })
+    })
+}
---
+++ models/violation/updateViolation_model.js
@@ -0,0 +1,24 @@
+const connect = require("../connection_db.js");
+
+/**
+ * 
+ * @param {{VContent: string, StuID: string, VNumber: int}} violation 
+ * @returns 
+ */
+module.exports = function updateViolation(violation) {
+    let result = {};
+    return new Promise((resolve, reject) => {
+        connect.query(`UPDATE VIOLATION SET VContent = ? WHERE StuID = ? AND VNumber = ?`, [violation.VContent, violation.StuID, violation.VNumber], (err) => {
+            if(err) {
+                result.status = "Failed!";
+                result.message = "更新違規紀錄失敗！";
+                reject(result);
+                return;
+            }
+            result.status = "Success!";
+            result.message = "更新違規紀錄成功！";
+            resolve(result);
+            return;
+        })
+    })
+}
