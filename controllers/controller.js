298224cd3be27180714bbe427f4131e67b8a778b
YeeKaiKai <kyle910204@gmail.com>
2022-12-26 23:17:43 +0800

    Add four message-method functions

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,11 @@
 const regist = require("../models/register_model.js");
 const login = require("../models/login_model.js");
 const verify = require("../models/verification.js");
-const addComment = require("../models/messageBoard/addComment_model.js");
+
+const addMessage = require("../models/messageBoard/addMessage_model.js");
+const removeMessage = require("../models/messageBoard/removeMessage_model.js");
+const viewMessage = require("../models/messageBoard/viewMessage_model.js");
+const updateMessage = require("../models/messageBoard/updateMessage_model.js");
 
 const jwt = require('jsonwebtoken');
 const config = require('../config/config.js');
@@ -9,7 +13,7 @@ const config = require('../config/config.js');
 /**
  *  Receive post method to regist
  */
-exports.postRegist = function(req, res) {
+exports.postRegist = function(req, res, next) {
     studentData = {
         StuID: "111", 
         SAccount: "222",
@@ -22,17 +26,19 @@ exports.postRegist = function(req, res) {
         res.json({
             result: result
         }) 
+        return;
     }).catch((err) => {
         res.json({
             err: err
         })
+        return;
     });
 }
 
 /**
  * Login for student
  */
-exports.postStudentLogin = function(req, res) {
+exports.postStudentLogin = function(req, res, next) {
     studentData = {
         SAccount: req.body.SAccount,
         SPassword: req.body.SPassword
@@ -41,6 +47,7 @@ exports.postStudentLogin = function(req, res) {
         // make token that is set expired after an hour, and let StuID be the token data
         let token = jwt.sign({
             algorithm: 'HS256',
+            // token expired after an hour 
             exp: Math.floor(Date.now() / 1000, (60 * 60)),
             data: result[0].StuID
         }, config.secret);
@@ -49,33 +56,99 @@ exports.postStudentLogin = function(req, res) {
         res.json({
             result: result,
         })
+        return;
     }).catch((err) => {
         res.json({ 
             err: err
         })
+        return;
     })
 }
 
-exports.postComment = function(req, res) {
+exports.postMessage = function(req, res, next) {
     let token = req.headers['token'];
     verify(token).then((data) => {
         let message = {
             StuID: data,
-            MContent: "%%%",
+            MContent: req.body.MContent,
         }
-        addComment(message).then((result) => {
+        addMessage(message).then((result) => {
             res.json({
                 result: result
             })
+            return;
         }).catch((err) => {
             res.json({
                 err: err
             })
+            return;
         })
     }).catch((err) => {
         res.json({
             err: err
         })
+        return;
     })
 }
 
+exports.deleteMessage = function(req, res, next) {
+    let token = req.headers['token'];
+    verify(token).then((data) => {
+        let message = {
+            StuID: data,
+            MNumber: req.body.MNumber
+        }
+        removeMessage(message).then((result) => {
+            res.json({
+                result: result
+            })
+            return;
+        }).catch((err) => {
+            res.json({
+                err: err
+            })
+            return;
+        })
+    }).catch((err) => {
+        res.json({
+            err: err
+        })
+        return;
+    })
+}
+
+exports.getMessage = function(req, res, next) {
+    viewMessage().then((rows) => {
+        res.json({
+            result: rows
+        })
+        return;
+    }).catch((err) => {
+        res.json({
+            result: err
+        })
+        return;
+    })
+}
+
+exports.putMessage = function(req, res, next) {
+    let token = req.headers['token'];
+    verify(token).then((data) => {
+        let message = {
+            StuID: data,
+            MContent: req.body.MContent,
+            MNumber: req.body.MNumber
+        }
+    })
+    updateMessage(message).then((result) => {
+        res.json({
+            result: result
+        })
+        return;
+    }).catch((err) => {
+        res.json({
+            err: err
+        })
+        return;
+    })
+}
