95c05b7575d5925098c543bcea60d909b2a7bfbe
YeeKaiKai <kyle910204@gmail.com>
2022-12-28 10:41:30 +0800

    Remove the part which is used to check if it is expired of token

--- models/verification.js
+++ models/verification.js
@@ -8,17 +8,11 @@ const config = require('../config/config.js');
  */
 module.exports = function(token) {
     let result = {};
-    let time = Math.floor(Date.now() / 1000);
     return new Promise((resolve, reject) => {
         jwt.verify(token, config.secret, (err, decoded) => {
             if(err) {
                 result.status = "Failed!";
                 result.message = "Token驗證失敗！";
-                reject(result);
-                return;
-            } else if (decoded.exp < time) {
-                result.status = "Failed!";
-                result.message = "Token過期！";
                 reject(result);
                 return;
             } else {
