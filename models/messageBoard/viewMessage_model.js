8ce2d7838b265b0ccfcc74e6b3b51bcfda554284
YeeKaiKai <kyle910204@gmail.com>
2023-01-03 23:09:15 +0800

    Update after database changing

--- models/messageBoard/viewMessage_model.js
+++ models/messageBoard/viewMessage_model.js
@@ -7,8 +7,9 @@ const connect = require("../connection_db.js");
 module.exports = function() {
     result = {};
     return new Promise((resolve, reject) => {
-        connect.query(`SELECT MContent, SName FROM MESSAGE NATURAL JOIN STUDENT`, (err, rows) => {
+        connect.query(`SELECT MContent, MNumber, UName FROM MESSAGE LEFT JOIN STUDENT USING(StuID) INNER JOIN USER ON STUDENT.StuID = USER.UID`, (err, rows) => {
             if(err) {
+                console.log(err);
                 result.status = "Failed!";
                 result.message = "瀏覽留言板失敗！";
                 reject(result);
