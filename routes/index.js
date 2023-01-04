b4351f133212fd72fdb5a0f2caee51674dae4e8a
YeeKaiKai <kyle910204@gmail.com>
2023-01-04 12:34:35 +0800

    Add router of announcement and others that are missed

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/register', controller.postRegist);
 router.post('/login', controller.postLogin);
 
 router.post('/messageBoard', controller.postMessage);
 router.delete('/messageBoard', controller.deleteMessage);
+router.get('/messageBoard', controller.getMessage);
+router.put('/messageBoard', controller.putMessage);
+
+router.post('/announcement', controller.postAnnouncement);
+router.delete('/announcement', controller.deleteAnnouncement);
+// router.get('/announcement', controller.getAnnouncement);
+router.put('/announcement', controller.putAnnouncement);
 
 module.exports = router;
