ab42129a0461279ffe72c46b0371bd82153592a3
YeeKaiKai <kyle910204@gmail.com>
2023-01-04 20:04:02 +0800

    Fix error in query

--- models/dormitory/removeFacility_model.js
+++ models/dormitory/removeFacility_model.js
@@ -7,9 +7,10 @@ const connect = require("../connection_db.js");
 module.exports = function(facility) {
     let result = {};
     return new Promise((resolve, reject) => {
-        let sql = `DELETE FROM ROOM WHERE DName = "${facility.DName}" AND RoomNumber = "${facility.RoomNumber}" AND FName = ${facility.FName}`;
+        let sql = `DELETE FROM FACILITY WHERE DName = "${facility.DName}" AND RoomNumber = "${facility.RoomNumber}" AND FName = "${facility.FName}" `;
         connect.query(sql, (err) => {
             if(err) {
+                console.log(err);
                 result.status = "Failed!";
                 result.message = "刪除設備資料失敗，伺服器錯誤！";
                 reject(result);
