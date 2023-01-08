8c4a4ba6b31a079a6ab8457b7022cbd505dd3d67
YeeKaiKai <kyle910204@gmail.com>
2023-01-08 12:35:20 +0800

    Add front-end student message board

---
+++ public/javascripts/student_message_board.js
@@ -0,0 +1,54 @@
+global_url = "http://192.168.1.112:3000/student/messageBoard/"
+
+
+// delete
+function deleteItem(pk) {
+  const url = global_url
+  let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+  let body = {
+    "MNumber": pk,
+  }
+  console.log(body)
+
+  fetch(url, {
+      method: "DELETE",
+      headers: headers,
+      body: JSON.stringify(body)
+  }).then((response) => {console.log(response)})
+  .catch(json => {
+    console.log(json)
+
+  });
+
+  window.location.reload();
+}
+
+
+
+// update
+function updateItem(pk) {
+  const url = global_url
+  let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+  let body = {
+    "MNumber": pk,
+    "MContent": document.getElementById("MContent").value,
+  }
+  console.log(body)
+
+  fetch(url, {
+      method: "PUT",
+      headers: headers,
+      body: JSON.stringify(body)
+  }).then(response => response.json())
+  .catch(json => console.log(json));
+
+  window.location.reload();
+}
+
+
