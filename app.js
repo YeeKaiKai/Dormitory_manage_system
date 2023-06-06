6f3a4c6f909413a54c3878f57060a71d3b0003a4
YeeKaiKai <kyle910204@gmail.com>
2023-06-06 22:36:04 +0800

    fix: cannot read static files

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ var housemasterRouter = require('./routes/housemaster');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/routes/public')));
 
 app.use('/', indexRouter);
 app.use('/student/', studentRouter);
