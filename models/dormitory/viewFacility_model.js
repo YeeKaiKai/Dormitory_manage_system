4c94dc4d5fff8264f36cbd5a2050b370e6f1384f
YeeKaiKai <kyle910204@gmail.com>
2023-06-09 00:33:27 +0800

    feat: add view facility model

---
+++ models/dormitory/viewFacility_model.js
@@ -0,0 +1,28 @@
+const connect = require("../connection_db.js");
+const check = require("../../service/check.js");
+
+/**
+ * 
+ * @param {{DName: string, RoomNumber: string}} facility 
+ * @returns 
+ */
+module.exports = function(facility) {
+    let result = {};
+    return new Promise((resolve, reject) => {
+        let sql = `SELECT *
+                   FROM FACILITY
+                   WHERE DName="${facility.DName} AND RoomNumber="${facility.RoomNumber}"`;
+        connect.query(sql , (err, rows) => {
+            if(err) {
+                result.status = false;
+                result.message = "瀏覽設備資料失敗！";
+                reject(result);
+                return;
+            }
+            console.log(sql);
+            let data = JSON.stringify(rows);
+            resolve(data);
+            return;
+        })
+    })
+}
