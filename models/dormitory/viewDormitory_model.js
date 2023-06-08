61ae0a33e8a652fbd33665550bc714123cc27a8d
YeeKaiKai <kyle910204@gmail.com>
2023-06-09 00:16:39 +0800

    update: view dormitory model now just have dormitory information

--- models/dormitory/viewDormitory_model.js
+++ models/dormitory/viewDormitory_model.js
@@ -9,34 +9,8 @@ const check = require("../../service/check.js");
 module.exports = function(dormitory) {
     let result = {};
     return new Promise((resolve, reject) => {
-        let sql;
-        // check if the inquiry has input
-        if(check.checkNull(dormitory.DName) === true) { // check all dormitory
-            sql = `
-            SELECT * 
-            FROM DORMITORY
-            NATURAL JOIN ROOM
-            NATURAL JOIN FACILITY`;
-        } else if (check.checkNull(dormitory.RoomNumber) === true) { // check all rooms in a dormitory
-            sql = `
-            SELECT * 
-            FROM DORMITORY
-            NATURAL JOIN ROOM
-            NATURAL JOIN FACILITY
-            WHERE DName = "${dormitory.DName}"`;
-        } else if (check.checkNull(dormitory.FName) === true){ // check all facilities in a room
-
-            sql = `
-            SELECT * 
-            FROM ROOM
-            NATURAL JOIN FACILITY
-            WHERE DName = "${dormitory.DName}" AND RoomNumber = "${dormitory.RoomNumber}"`;
-        } else { // check a facility in a room
-            sql = `
-            SELECT *
-            FROM FACILITY
-            WHERE DName = "${dormitory.DName}" AND RoomNumber = "${dormitory.RoomNumber}" AND FName = "${dormitory.FName}"`;
-        }
+        let sql = `SELECT *
+                   FROM DORMITORY`;
         connect.query(sql , (err, rows) => {
             if(err) {
                 result.status = false;
