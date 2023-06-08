a58995dfd8ad17b050ecbfa21fb7a9c16013d436
guo <b22790188@gmail.com>
2023-06-09 01:49:57 +0800

    feat: add a new model used to insert repairform

---
+++ models/repairForm/makeRepairForm.js
@@ -0,0 +1,26 @@
+const connect = require("../connection_db.js");
+
+/**
+ * Make the RepairForm by student
+ * @param {{UID: String, FName: CharacterData, RoomNumber: CharacterData, DName: CharacterData}}
+ * @returns
+ */
+
+module.exports = function(repairForm) {
+    let result = {};
+    return new Promise((resolve, reject) => {
+        connect.query(`INSERT INTO REPAIR_FORM(UID, FName, RoomNumber, DName) VALUES(?, ?, ?, ?)`, [repairForm.UID, repairForm.FName, repairForm.RoomNumber, repairForm.DName], (err) => {
+            if(err){
+                console.log(err);
+                result.status = false;
+                result.message = "報修申請失敗!";
+                reject(result);
+                return;
+            }
+            result.status = true;
+            result.message = "申請成功!";
+            resolve(result);
+            return;
+        })
+    })
+}
