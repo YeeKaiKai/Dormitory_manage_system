1dfde4d0a55f65cf617a4dc251536172020114ed
guo <b22790188@gmail.com>
2023-06-09 23:32:30 +0800

    update: insert new column RepairMsg

--- models/repairForm/makeRepairForm.js
+++ models/repairForm/makeRepairForm.js
@@ -2,14 +2,14 @@ const connect = require("../connection_db.js");
 
 /**
  * Make the RepairForm by student
- * @param {{UID: String, FName: CharacterData, RoomNumber: CharacterData, DName: CharacterData}}
+ * @param {{UID: String, FName: CharacterData, RoomNumber: CharacterData, DName: CharacterData, RepairMsg: CharacterData}}
  * @returns
  */
 
 module.exports = function(repairForm) {
     let result = {};
     return new Promise((resolve, reject) => {
-        connect.query(`INSERT INTO REPAIR_FORM(UID, FName, RoomNumber, DName) VALUES(?, ?, ?, ?)`, [repairForm.UID, repairForm.FName, repairForm.RoomNumber, repairForm.DName], (err) => {
+        connect.query(`INSERT INTO REPAIR_FORM(UID, FName, RoomNumber, DName, RepairMsg) VALUES(?, ?, ?, ?, ?)`, [repairForm.UID, repairForm.FName, repairForm.RoomNumber, repairForm.DName, repairForm.RepairMsg], (err) => {
             if(err){
                 console.log(err);
                 result.status = false;
