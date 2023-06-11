f16bef8721b0c92704941d3ffb6ec1aee7931fd7
jiamin <alo2002719@gmail.com>
2023-06-11 15:04:03 +0800

    feat: 新增repair相關的js檔

---
+++ routes/public/javascripts/student_repair.js
@@ -0,0 +1,28 @@
+global_url = "http://localhost:3000/student/repairForm"
+
+// update
+function updateItem(pk) {
+    const url = global_url
+    let headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+    let body = {
+      "RepairNumber": pk,
+      "DName": document.getElementById("DName" + pk).value,
+      "RoomNumber": document.getElementById("RoomNumber" + pk).value,
+      "FName": document.getElementById("FName" + pk).value,
+      "RContent": document.getElementById("RContent" + pk).value,
+      "Freetime": document.getElementById("Freetime" + pk).value,
+    }
+    console.log(body)
+  
+    fetch(url, {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify(body)
+    }).then(response => response.json())
+    .catch(json => console.log(json));
+  
+    window.location.reload();
+}
