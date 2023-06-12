60528f95ab1a02c2f7e2246b2ba31101e38e65a7
YeeKaiKai <kyle910204@gmail.com>
2023-06-13 01:57:47 +0800

    feat: add AType in sql query

--- models/application/makeApplication_model.js
+++ models/application/makeApplication_model.js
@@ -2,14 +2,14 @@ const connect = require("../connection_db.js");
 
 /**
  * Make the application by student
- * @param {{StuID: string, DName: string, ApplyAcademicYear: int, ApplySemester: char}} application 
+ * @param {{StuID: string, DName: string, ApplyAcademicYear: int, ApplySemester: char, AType: string}} application 
  * @returns 
  */
 module.exports = function(application) {
     let result = {};
     return new Promise((resolve, reject) => {
         console.log(application);
-        connect.query(`INSERT INTO APPLICATION(DATE, StuID, DName) VALUES((SELECT CONVERT_TZ(CURRENT_TIME(),'+00:00','+08:00')), ?, ?)`, [application.StuID, application.DName], (err) => {
+        connect.query(`INSERT INTO APPLICATION(DATE, StuID, DName, AType) VALUES((SELECT CONVERT_TZ(CURRENT_TIME(),'+00:00','+08:00')), ?, ?, ?)`, [application.StuID, application.DName, application.AType], (err) => {
             if(err) {
                 console.log(err);
                 result.status = false;
