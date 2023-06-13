41799c42e71b5b999f125544d1edb92f667e067d
trytrylookk <trytrylookk@gmail.com>
2023-06-13 20:41:07 +0800

    style: update some buttons and formatting about admin apply

--- routes/public/javascripts/admin_apply.js
+++ routes/public/javascripts/admin_apply.js
@@ -28,8 +28,8 @@ function updateItem_in(pk1, pk2, pk3, pk4) {
   window.location.reload();
 }
 
-// update exc/out
-function updateItem_eo(pk1, pk2, pk3, pk4, pk5) {
+// update exc
+function updateItem_exc(pk1, pk2, pk3, pk4, pk5) {
   const url = global_url
   console.log(url);
   let headers = {
@@ -43,6 +43,33 @@ function updateItem_eo(pk1, pk2, pk3, pk4, pk5) {
     "DName":pk3,
     "RoomNumber":pk4,
     "AType":pk5
+  }
+  console.log(body)
+
+  fetch(url, {
+      method: "PUT",
+      headers: headers,
+      body: JSON.stringify(body)
+  }).then(response => response.json())
+  .catch(json => console.log(json));
+
+  window.location.reload();
+}
+
+// update out
+function updateItem_out(pk1, pk2, pk3, pk4) {
+  const url = global_url
+  console.log(url);
+  let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+  let body = {
+    "ApplyNumber": pk1,
+    "StuID":pk2,
+    "Approved":document.getElementById("Approved" + pk1 + pk2).value,
+    "DName":pk3,
+    "AType":pk4
   }
   console.log(body)
 
