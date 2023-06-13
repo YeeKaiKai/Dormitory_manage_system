9cee318f018b8ef0be1440475c083a6446050ae3
trytrylookk <trytrylookk@gmail.com>
2023-06-13 14:41:58 +0800

    update: some modification for application subsystem

--- routes/public/javascripts/admin_apply.js
+++ routes/public/javascripts/admin_apply.js
@@ -27,10 +27,34 @@ global_url = "http://localhost:3000/admin/application/"
 //   window.location.reload();
 // }
 
+// update in
+function updateItem_in(pk1, pk2, pk3) {
+  const url = global_url
+  console.log(url);
+  let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+  let body = {
+    "ApplyNumber": pk1,
+    "StuID":pk2,
+    "Approved":document.getElementById("Approved" + pk1 + pk2).value,
+    "DName":pk3,
+    // "Paid":document.getElementById("Paid").value
+  }
+  console.log(body)
 
+  fetch(url, {
+      method: "PUT",
+      headers: headers,
+      body: JSON.stringify(body)
+  }).then(response => response.json())
+  .catch(json => console.log(json));
 
-// update
-function updateItem(pk1, pk2, pk3) {
+  window.location.reload();
+}
+// update exc/out
+function updateItem_eo(pk1, pk2, pk3) {
   const url = global_url
   console.log(url);
   let headers = {
