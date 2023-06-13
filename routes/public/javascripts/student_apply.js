3a36d35e139bcfdd5803027b722d9dc541f0c498
trytrylookk <trytrylookk@gmail.com>
2023-06-13 17:55:32 +0800

    update: 僅申請換宿需輸入房號

--- routes/public/javascripts/student_apply.js
+++ routes/public/javascripts/student_apply.js
@@ -1,13 +1,16 @@
 var typeSelect = document.querySelector('select[name="AType"]'); 
 var roomLabel = document.querySelector('label[for="inputRoom"]');
-var roomInput = document.querySelector('input[name="Room"]');
-                        
+var roomInput = document.querySelector('input[name="ARoomNumber"]');
+
+roomLabel.style.display = 'none';
+roomInput.style.display = 'none';
+
 typeSelect.addEventListener('change', function() {
-    if (typeSelect.value === '換宿申請' || typeSelect.value === '退宿申請') {
-        roomLabel.style.display = 'block';
-        roomInput.style.display = 'block';
-    } else {
+    if (typeSelect.value === '申請住宿' || typeSelect.value ==='申請退宿') {
         roomLabel.style.display = 'none';
         roomInput.style.display = 'none';
+    } else {
+        roomLabel.style.display = 'block';
+        roomInput.style.display = 'block';
     }
 });
